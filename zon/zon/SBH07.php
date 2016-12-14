f0c318afa7db44f1b024b1169d08445f22a704b7
Mohd Zul Haziq <xidontknow.phsh@gmail.com>
2016-12-14 22:29:07 +0800

    memperbetulkan kesilapan api

---
+++ zon/zon/SBH07.php
@@ -0,0 +1,55 @@
+<?php
+
+/* 
+	Waktu Solat Sabah Zon 7 - Papar, Ranau, Kota Belud, Tuaran, Penampang, Kota Kinabalu
+	Salam sayang dari Zul
+	Facebook: www.facebook.com/xidontknow
+	Twitter: https://twitter.com/xidontknowmy
+	Website: http://www.mohdzulhaziq.my 
+*/
+
+$api = 'https://api.jomgeek.com/v1/waktusolat/?k=yEoNjYQ7K8tqImHN&a=SBH07';
+$json = file_get_contents($api);
+$retVal = json_decode($json, TRUE);
+
+print 'Zon: Sabah 7 - Papar, Ranau, Kota Belud, Tuaran, Penampang, Kota Kinabalu<br>';
+
+print 'Tarikh: ';
+$tarikh = $retVal['data']['date'];
+print $tarikh;
+print '<br>';
+
+print 'Imsak: ';
+$imsak = $retVal['data']['item']['Imsak'];
+print $imsak;
+print '<br>';
+
+print 'Subuh: ';
+$subuh = $retVal['data']['item']['Subuh'];
+print $subuh;
+print '<br>';
+
+print 'Syuruk: ';
+$syuruk = $retVal['data']['item']['Syuruk'];
+print $syuruk;
+print '<br>';
+
+print 'Zohor: ';
+$zohor = $retVal['data']['item']['Zohor'];
+print $zohor;
+print '<br>';
+
+print 'Asar: ';
+$asar = $retVal['data']['item']['Asar'];
+print $asar;
+print '<br>';
+
+print 'Maghrib: ';
+$maghrib = $retVal['data']['item']['Maghrib'];
+print $maghrib;
+print '<br>';
+
+print 'Isyak: ';
+$isyak = $retVal['data']['item']['Isyak'];
+print $isyak;
+print '<br>';
