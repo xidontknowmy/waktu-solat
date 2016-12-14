0a14fd2c6527964f38f66edd54469e7f090b015a
Mohd Zul Haziq <xidontknow.phsh@gmail.com>
2016-12-14 22:00:35 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,289 @@
+<title>Waktu Solat Malaysia</title>
+<link rel="stylesheet" href="resources/css/seocss.css" />
+        <link rel="stylesheet" href="resources/css/default.css" />
+        <link rel="stylesheet" href="resources/css/responsive.css" />
+        <link rel="stylesheet" href="resources/css/toastr.min.css" />    
+        <link rel="stylesheet" href="resources/css/jquery-ui.min.css" />
+		
+<header class="header">
+            <div class="header-logo-container">
+                
+            </div>
+            <div class="header-title-container">
+                <h1 class="header-title">Waktu Solat Malaysia</h1>
+                <p class="header-description">Dah ada benda ni, jangan pandai pandai tinggal solat pulekkk.</p>
+            </div>
+            <div class="header-user-container"></div>
+        </header>
+		
+        <section class="section section-default">
+    <div class="section-title">
+        <h2>Waktu Solat</h2>
+    </div>
+    <div class="section-body center-align">
+        <div class="section-content">Pilih Zon Kawasan Anda</div>
+        <div class="section-content">
+		<form action="" method="post">
+            <span class="bold">Zon:</span>
+           <select name="zon">
+  <option value="#">==========        PILIH        ==========</option>
+  <option value="KDH01">Kedah Zon 1: Kota Setar, Kubang Pasu, Pokok Sena</option>
+  <option value="KDH02">Kedah Zon 2: Pendang, Kuala Muda, Yan</option>
+  <option value="KDH03">Kedah Zon 3: Padang Terap, Sik</option>
+  <option value="KDH04">Kedah Zon 4: Baling</option>
+  <option value="KDH05">Kedah Zon 5: Kulim, Bandar Bahru</option>
+  <option value="KDH06">Kedah Zon 6: Langkawi</option>
+  <option value="KDH07">Kedah Zon 7: Gunung Jerai</option>
+  <option value="PNG01">P. Pinang: Seluruh Negeri Pulau Pinang</option>
+  <option value="PRK01">Perak Zon 1: Tapah,Slim River dan Tanjung Malim</option>
+  <option value="PRK02">Perak Zon 2: Ipoh, Batu Gajah, Kampar, Sg. Siput dan Kuala Kangsar</option>
+  <option value="PRK03">Perak Zon 3: Pengkalan Hulu, Grik dan Lenggong</option>
+  <option value="PRK04">Perak Zon 4: Temengor dan Belum</option>
+  <option value="PRK05">Perak Zon 5: Teluk Intan, Bagan Datoh, Kg.Gajah,Sri Iskandar, Beruas,Parit,Lumut,Setiawan dan Pulau Pangkor</option>
+  <option value="PRK06">Perak Zon 6: Selama, Taiping, Bagan Serai dan Parit Buntar</option>
+  <option value="PRK07">Perak Zon 7: Bukit Larut</option>
+  <option value="SGR01">Selangor Zon 1: Gombak,H.Selangor,Rawang, H.Langat,Sepang,Petaling, S.Alam</option>
+  <option value="SGR02">Sabak Bernam, Kuala Selangor, Klang, Kuala Langat</option>
+  <option value="SGR03">Kuala Lumpur</option>
+  <option value="SGR04">Putrajaya</option>
+  <option value="WLY02">Labuan</option>
+  <option value="NGS01">N. Sembilan Zon 1: Jempol, Tampin</option>
+  <option value="NGS02">N. Sembilan Zon 2: Port Dickson, Seremban, Kuala Pilah, Jelebu, Rembau</option>
+  <option value="MLK01">Melaka: Bandar Melaka, Alor Gajah, Jasin, Masjid Tanah, Merlimau, Nyalas</option>
+  <option value="JHR01">Johor Zon 1: Pulau Aur dan Pemanggil</option>
+  <option value="JHR02">Johor Zon 2: Kota Tinggi, Mersing, Johor Bahru</option>
+  <option value="JHR03">Johor Zon 3: Kluang dan Pontian</option>
+  <option value="JHR04">Johor Zon 4: Batu Pahat, Muar, Segamat, Gemas</option>
+  <option value="PLS01">Perlis: Kangar, Padang Besar, Arau</option>
+  <option value="PHG01">Pahang Zon 1: Pulau Tioman</option>
+  <option value="PHG02">Pahang Zon 2: Kuantan, Pekan, Rompin, Muadzam Shah</option>
+  <option value="PHG03">Pahang Zon 3: Maran, Chenor, Temerloh, Bera, Jerantut</option>
+  <option value="PHG04">Pahang Zon 4: Bentong, Raub, Kuala Lipis</option>
+  <option value="PHG05">Pahang Zon 5: Genting Sempah, Janda Baik, Bukit Tinggi</option>
+  <option value="PHG06">Pahang Zon 6: Bukit Fraser, Genting Higlands, Cameron Higlands</option>
+  <option value="KTN01">Kelantan Zon 1: K.Bharu,Bachok,Pasir Puteh,Tumpat,Pasir Mas,Tnh. Merah,Machang,Kuala Krai,Mukim Chiku</option>
+  <option value="KTN02">Kelantan Zon 2: Jeli, Gua Musang (Mukim Galas, Bertam)</option>
+  <option value="TRG01">Terengganu Zon 1: Kuala Terengganu, Marang</option>
+  <option value="TRG02">Terengganu Zon 2: Besut, Setiu</option>
+  <option value="TRG03">Terengganu Zon 3: Hulu Terengganu</option>
+  <option value="TRG04">Terengganu Zon 4: Kemaman Dungun</option>
+  <option value="SBH01">Sabah Zon 1: Sandakan, Bdr. Bkt. Garam, Semawang, Temanggong, Tambisan</option>
+  <option value="SBH02">Sabah Zon 2: Pinangah, Terusan, Beluran, Kuamut, Telupit</option>
+  <option value="SBH03">Sabah Zon 3: Lahad Datu, Kunak, Silabukan, Tungku, Sahabat, Semporna</option>
+  <option value="SBH04">Sabah Zon 4: Tawau, Balong, Merotai, Kalabakan</option>
+  <option value="SBH05">Sabah Zon 5: Kudat, Kota Marudu, Pitas, Pulau Banggi</option>
+  <option value="SBH06">Sabah Zon 6: Gunung Kinabalu</option>
+  <option value="SBH07">Sabah Zon 7: Papar, Ranau, Kota Belud, Tuaran, Penampang, Kota Kinabalu</option>
+  <option value="SBH08">Sabah Zon 8: Pensiangan, Keningau, Tambunan, Nabawan</option>
+  <option value="SBH09">Sabah Zon 9: Sipitang, Membakut, Beaufort, Kuala Penyu, Weston, Tenom, Long Pa Sia</option>
+  <option value="SWK01">Sarawak Zon 1: Limbang, Sundar, Trusan, Lawas</option>
+  <option value="SWK02">Sarawak Zon 2: Niah, Bekenu, Miri, Sibuti, Marudi</option>
+  <option value="SWK03">Sarawak Zon 3: Tatau, Suai, Belaga, Pandan, Sebauh, Bintulu</option>
+  <option value="SWK04">Sarawak Zon 4: Igan, Oya, Balingian, Mukah, Dalat, Sibu, Kanowit, Kapit, Song</option>
+  <option value="SWK05">Sarawak Zon 5: Tanjung Manis, Belawai, Matu, Daro, Sarikei, Julau, Bintangor, Rajang</option>
+  <option value="SWK06">Sarawak Zon 6: Kabong, Lingga, Sri Aman, Engkelili, Lubok Antu, Betong, Spaoh, Pusa, Saratok, Roban, Debak</option>
+  <option value="SWK07">Sarawak Zon 7: Samarahan, Simunjan, Serian, Sebuyau, Meludam</option>
+  <option value="SWK08">Sarawak Zon 8: Kuching, Bau, Lundu,Sematan</option>
+  <option value="SWK09">Sarawak Zon 9: Zon Khas</option>
+  
+</select>
+  <br><br>
+            <div class="btn-group">
+                <button input type="Submit" name="papar" class="btn btn-default">Papar</button>
+                </form>
+				<br><br><br><br><br><br><br>
+				
+<?php
+if(isset($_POST['zon'])){
+    $zon = $_POST['zon'];
+    switch ($zon) {
+case 'KDH01':
+            echo file_get_contents("http://localhost/zon/KDH01.php");
+            break;
+case 'KDH02':
+            echo file_get_contents("http://localhost/zon/KDH02.php");
+            break;
+case 'KDH03':
+            echo file_get_contents("http://localhost/zon/KDH03.php");
+            break;
+case 'KDH04':
+            echo file_get_contents("http://localhost/zon/KDH04.php");
+            break;
+case 'KDH05':
+            echo file_get_contents("http://localhost/zon/KDH05.php");
+            break;
+case 'KDH06':
+            echo file_get_contents("http://localhost/zon/KDH06.php");
+            break;
+case 'KDH07':
+            echo file_get_contents("http://localhost/zon/KDH07.php");
+            break;
+case 'PNG01':
+            echo file_get_contents("http://localhost/zon/PNG01.php");
+            break;
+case 'PRK01':
+            echo file_get_contents("http://localhost/zon/PRK01.php");
+            break;
+case 'PRK02':
+            echo file_get_contents("http://localhost/zon/PRK02.php");
+            break;
+case 'PRK03':
+            echo file_get_contents("http://localhost/zon/PRK03.php");
+            break;
+case 'PRK04':
+            echo file_get_contents("http://localhost/zon/PRK04.php");
+            break;
+case 'PRK05':
+            echo file_get_contents("http://localhost/zon/PRK05.php");
+            break;
+case 'PRK06':
+            echo file_get_contents("http://localhost/zon/PRK06.php");
+            break;
+case 'PRK07':
+            echo file_get_contents("http://localhost/zon/PRK07.php");
+            break;
+case 'SGR01':
+            echo file_get_contents("http://localhost/zon/SGR01.php");
+            break;
+case 'SGR02':
+            echo file_get_contents("http://localhost/zon/SGR02.php");
+            break;
+case 'SGR03':
+            echo file_get_contents("http://localhost/zon/SGR03.php");
+            break;
+case 'SGR04':
+            echo file_get_contents("http://localhost/zon/SGR04.php");
+            break;
+case 'WLY02':
+            echo file_get_contents("http://localhost/zon/WLY2.php");
+            break;
+case 'NGS01':
+            echo file_get_contents("http://localhost/zon/NGS01.php");
+            break;
+case 'NGS02':
+            echo file_get_contents("http://localhost/zon/NGS02.php");
+            break;
+case 'MLK01':
+            echo file_get_contents("http://localhost/zon/MLK01.php");
+            break;
+case 'JHR01':
+            echo file_get_contents("http://localhost/zon/JHR01.php");
+            break;
+case 'JHR02':
+            echo file_get_contents("http://localhost/zon/JHR02.php");
+            break;
+case 'JHR03':
+            echo file_get_contents("http://localhost/zon/JHR03.php");
+            break;
+case 'JHR04':
+            echo file_get_contents("http://localhost/zon/JHR04.php");
+            break;
+case 'PLS01':
+            echo file_get_contents("http://localhost/zon/PLS01.php");
+            break;
+case 'PHG01':
+            echo file_get_contents("http://localhost/zon/PHG01.php");
+            break;
+case 'PHG02':
+            echo file_get_contents("http://localhost/zon/PHG02.php");
+            break;
+case 'PHG03':
+            echo file_get_contents("http://localhost/zon/PHG03.php");
+            break;
+case 'PHG04':
+            echo file_get_contents("http://localhost/zon/PHG04.php");
+            break;
+case 'PHG05':
+            echo file_get_contents("http://localhost/zon/PHG05.php");
+            break;
+case 'PHG06':
+            echo file_get_contents("http://localhost/zon/PHG06.php");
+            break;
+case 'KTN01':
+            echo file_get_contents("http://localhost/zon/KTN01.php");
+            break;
+case 'KTN02':
+            echo file_get_contents("http://localhost/zon/KTN02.php");
+            break;
+case 'TRG01':
+            echo file_get_contents("http://localhost/zon/TRG01.php");
+            break;
+case 'TRG02':
+            echo file_get_contents("http://localhost/zon/TRG02.php");
+            break;
+case 'TRG03':
+            echo file_get_contents("http://localhost/zon/TRG03.php");
+            break;
+case 'TRG04':
+            echo file_get_contents("http://localhost/zon/TRG04.php");
+            break;
+case 'SBH01':
+            echo file_get_contents("http://localhost/zon/SBH01.php");
+            break;
+case 'SBH02':
+            echo file_get_contents("http://localhost/zon/SBH02.php");
+            break;
+case 'SBH03':
+            echo file_get_contents("http://localhost/zon/SBH03.php");
+            break;
+case 'SBH04':
+            echo file_get_contents("http://localhost/zon/SBH04.php");
+            break;
+case 'SBH05':
+            echo file_get_contents("http://localhost/zon/SBH05.php");
+            break;
+case 'SBH06':
+            echo file_get_contents("http://localhost/zon/SBH06.php");
+            break;
+case 'SBH07':
+            echo file_get_contents("http://localhost/zon/SBH07.php");
+            break;
+case 'SBH08':
+            echo file_get_contents("http://localhost/zon/SBH08.php");
+            break;
+case 'SBH09':
+            echo file_get_contents("http://localhost/zon/SBH09.php");
+            break;
+				
+case 'SWK01':
+            echo file_get_contents("http://localhost/zon/SWK01.php");
+            break;
+case 'SWK02':
+            echo file_get_contents("http://localhost/zon/SWK02.php");
+            break;
+case 'SWK03':
+            echo file_get_contents("http://localhost/zon/SWK03.php");
+            break;
+case 'SWK04':
+            echo file_get_contents("http://localhost/zon/SWK04.php");
+            break;
+case 'SWK05':
+            echo file_get_contents("http://localhost/zon/SWK05.php");
+            break;
+case 'SWK06':
+            echo file_get_contents("http://localhost/zon/SWK06.php");
+            break;
+case 'SWK07':
+            echo file_get_contents("http://localhost/zon/SWK07.php");
+            break;
+case 'SWK08':
+            echo file_get_contents("http://localhost/zon/SWK08.php");
+            break;
+case 'SWK09':
+            echo file_get_contents("http://localhost/zon/SWK09.php");
+            break;
+
+     
+    }
+}
+?>
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><center> <b>| Hak Cipta Terpelihara | Dicipta oleh <a href="http://www.facebook.com/xidontknow">Mohd Zul Haziq</a></b> |</center><br>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+		
